8bb9a66522c547d7371521e1ff1066ff12c3f7bc
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-18 22:05:04 +0800

    get single task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -20,4 +20,17 @@ const getTasks = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks };
+// GET PARTICULAR TASK
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json(`No task with id ${req.params.id}`);
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createTask, getTasks, getTask };
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask, getTasks } = require("../controllers/taskController");
+const { createTask, getTasks, getTask } = require("../controllers/taskController");
 
 // CREATE TASK
 router.post("/tasks", createTask);
 
 // GET ALL TASKS
 router.get("/tasks", getTasks);
+
+// GET PARTICULAR TASK
+router.get("/tasks/:id", getTask)
 
 module.exports = router;
